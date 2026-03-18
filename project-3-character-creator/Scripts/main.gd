4deec4d5ff5a9eea29758ad77cd54fa0134f0e21
Icarus003 <Bradfost03@gmail.com>
2026-03-18 14:45:09 -0400

    Started project, added scripts

---
+++ project-3-character-creator/Scripts/main.gd
@@ -0,0 +1,59 @@
+extends Node2D
+
+var heads = [
+	"head1",
+	"head2",
+	"head3",
+	"head4",
+	"head5"
+]
+
+var torsos = [
+	"torso1",
+	"torso2",
+	"torso3",
+	"torso4",
+	"torso5"
+]
+
+var legs = [
+	"leg1",
+	"leg2",
+	"leg3",
+	"leg4",
+	"leg5"
+]
+
+func change_head():
+	for h in legs:
+		if 
+
+func change_torso():
+	pass
+
+func change_legs():
+	pass
+
+
+func _on_hair_back_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_hair_forward_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_torso_back_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_torso_forward_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_legs_back_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_legs_forward_pressed() -> void:
+	pass # Replace with function body.
