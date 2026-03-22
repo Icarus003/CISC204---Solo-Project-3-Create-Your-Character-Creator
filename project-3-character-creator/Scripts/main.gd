c6a1e3e3e93e66db6a9d3402ea8927f6c25ee718
Icarus003 <Bradfost03@gmail.com>
2026-03-22 18:16:39 -0400

    Added complete array functionality

--- project-3-character-creator/Scripts/main.gd
+++ project-3-character-creator/Scripts/main.gd
@@ -1,48 +1,40 @@
 extends Node2D
 
-# Character attribute arrays
-var heads = [
+# Customizable object arrays
+var beds = [
 ]
 
-var torsos = [
-	"torso1",
-	"torso2",
-	"torso3",
-	"torso4",
-	"torso5"
+var paintings = [
 ]
 
-var legs = [
-	"leg1",
-	"leg2",
-	"leg3",
-	"leg4",
-	"leg5"
+var rugs = [
 ]
 
 # Current index for arrays
-var current_heads_index: int = 0
-var current_torsos_index: int = 0
-var current_legs_index: int = 0
+var current_beds_index: int = 0
+var current_paintings_index: int = 0
+var current_rugs_index: int = 0
+
 
 func _ready():
-	heads = [$Heads/Head1,$Heads/Head2,$Heads/Head3,$Heads/Head4,$Heads/Head5]
+	beds = [$RoomElements/Beds/BlueBed,$RoomElements/Beds/GreenBed,$RoomElements/Beds/OrangeBed,$RoomElements/Beds/PinkBed,$RoomElements/Beds/PurpleBed]
+	paintings = [$RoomElements/Paintings/Painting01,$RoomElements/Paintings/Painting02,$RoomElements/Paintings/Painting03,$RoomElements/Paintings/Painting04,$RoomElements/Paintings/Painting05]
+	rugs = [$RoomElements/Rugs/Rug01,$RoomElements/Rugs/Rug02,$RoomElements/Rugs/Rug03,$RoomElements/Rugs/Rug04,$RoomElements/Rugs/Rug05]
 	update_arrays()
 
-# Sets the initial visibility state for the character creator
+# Sets the initial visibility state for the room creator
 func update_arrays():
-	for h in heads:
-		h.visible = false
-	#for t in torsos:
-	#	t.visible = false
-	#for l in legs:
-	#	l.visible = false
-	
-	heads[current_heads_index].visible = true
-	#torsos[current_torsos_index].visbile = true
-	#legs[current_legs_index].visible = true
+	for b in beds:
+		b.visible = false
+	for p in paintings:
+		p.visible = false
+	for r in rugs:
+		r.visible = false
+	beds[current_beds_index].visible = true
+	paintings[current_paintings_index].visible = true
+	rugs[current_rugs_index].visible = true
 
-# Changes current index forward & back
+# Functions that change current index forward & back
 func change_index_forward(current_index):
 	if current_index <= 3:
 		current_index += 1
@@ -58,31 +50,35 @@ func change_index_back(current_index):
 	return current_index
 
 
-# Allows buttons to change current index forward & back
-func _on_hair_back_pressed():
-	change_index_back(current_heads_index)
-	current_heads_index = change_index_back(current_heads_index)
+# Functions for buttons to change the current visible objects and changes the index for the arrays 
+func _on_bed_back_button_pressed():
+	change_index_back(current_beds_index)
+	current_beds_index = change_index_back(current_beds_index)
 	update_arrays()
-	print(current_heads_index)
 
-func _on_hair_forward_pressed():
-	change_index_forward(current_heads_index)
-	current_heads_index = change_index_forward(current_heads_index)
+func _on_bed_forward_button_pressed():
+	change_index_forward(current_beds_index)
+	current_beds_index = change_index_forward(current_beds_index)
 	update_arrays()
-	print(current_heads_index)
+	print(current_beds_index)
 
-func _on_torso_back_pressed():
-	change_index_back(current_torsos_index)
-	current_torsos_index = change_index_back(current_torsos_index)
+func _on_painting_back_button_pressed():
+	change_index_back(current_paintings_index)
+	current_paintings_index = change_index_back(current_paintings_index)
+	update_arrays()
 
-func _on_torso_forward_pressed():
-	change_index_forward(current_torsos_index)
-	current_torsos_index = change_index_forward(current_torsos_index)
+func _on_painting_forward_button_pressed():
+	change_index_forward(current_paintings_index)
+	current_paintings_index = change_index_forward(current_paintings_index)
+	update_arrays()
 
-func _on_legs_back_pressed():
-	change_index_back(current_legs_index)
-	current_legs_index = change_index_back(current_legs_index)
+func _on_rug_back_button_pressed():
+	change_index_back(current_rugs_index)
+	current_rugs_index = change_index_back(current_rugs_index)
+	update_arrays()
 
-func _on_legs_forward_pressed():
-	change_index_forward(current_legs_index)
-	current_legs_index = change_index_forward(current_legs_index)
+
+func _on_rug_forward_button_pressed():
+	change_index_forward(current_rugs_index)
+	current_rugs_index = change_index_forward(current_rugs_index)
+	update_arrays()
